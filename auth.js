6fb0c1a8c63888cbc3c326d648c6095a6dfe5523
TamaSmilikitiw <hendy3858@gmail.com>
2025-07-11 21:37:25 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,17 +1,10 @@
 const axios = require('axios');
 
 const RAW_URL = 'https://raw.githubusercontent.com/TamaRebirth/BypassBotGuwa/main/tokens.json';
-const GITHUB_TOKEN = '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'; // ganti dengan token GitHub kamu jika repo private
 
 async function isTokenValid(token) {
   try {
-    const res = await axios.get(RAW_URL, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-        'User-Agent': 'TamaBot'
-      }
-    });
-
+    const res = await axios.get(RAW_URL); // ✅ tanpa header Authorization
     const list = res.data.tokens || [];
     return list.includes(token);
   } catch (err) {
